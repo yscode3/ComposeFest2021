79af49362ea9d542898897d4059f2202d971e164
PleasantJ <ys.hazard@gmail.com>
2021-11-05 18:32:03 +0900

    First commit

---
+++ week 1-Jetpack Compose basics/app/src/main/java/ys/compose/basicsyscode/MainActivity.kt
@@ -0,0 +1,38 @@
+package ys.compose.basicsyscode
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import ys.compose.basicsyscode.ui.theme.BasicsYSCodeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsYSCodeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BasicsYSCodeTheme {
+        Greeting("Android")
+    }
+}
