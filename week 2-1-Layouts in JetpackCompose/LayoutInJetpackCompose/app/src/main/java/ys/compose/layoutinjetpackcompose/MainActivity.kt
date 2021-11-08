05fb0567669e478a6e4e5de94363661d7b5ddf8a
PleasantJ <ys.hazard@gmail.com>
2021-11-08 20:59:04 +0900

    First Commit

---
+++ week 2-1-Layouts in JetpackCompose/LayoutInJetpackCompose/app/src/main/java/ys/compose/layoutinjetpackcompose/MainActivity.kt
@@ -0,0 +1,38 @@
+package ys.compose.layoutinjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import ys.compose.layoutinjetpackcompose.ui.theme.LayoutInJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LayoutInJetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LayoutInJetpackComposeTheme {
+        Greeting("Android")
+    }
+}
