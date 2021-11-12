f7c8a275e35fda7bc2520e77cbe9d30c369503b1
PleasantJ <ys.hazard@gmail.com>
2021-11-12 14:15:16 +0900

    Step 7

--- week 2-1-Layouts in JetpackCompose/LayoutInJetpackCompose/app/src/main/java/ys/compose/layoutinjetpackcompose/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutInJetpackCompose/app/src/main/java/ys/compose/layoutinjetpackcompose/MainActivity.kt
@@ -3,27 +3,35 @@ package ys.compose.layoutinjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import kotlinx.coroutines.launch
 import ys.compose.layoutinjetpackcompose.ui.theme.LayoutInJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PhotographerCard()
+//            PhotographerCard()
+            LayoutsYSCode()
         }
     }
 }
@@ -61,12 +69,107 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
 
 @Preview
 @Composable
+fun LayoutsYSCode() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutYSCode")
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Settings, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContents(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp)
+        )
+    }
+}
+
+@Composable
+fun BodyContents(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts YSCode")
+//        SimpleList()
+//        Spacer(modifier = Modifier.width(15.dp))
+        ScrollingList()
+    }
+}
+
+@Composable
+fun SimpleList() {
+    val scrollState = rememberScrollState()
+    Column(
+        Modifier
+            .verticalScroll(scrollState)
+            .fillMaxWidth()
+    ) {
+        repeat(100) {
+            ImageListItem(it)
+        }
+    }
+}
+
+@Composable
+fun ScrollingList() {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(modifier = Modifier.padding(top = 15.dp)) {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text(text = "Scroll to the top")
+            }
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize-1)
+                }
+            }) {
+                Text(text = "Scroll to the Bottom")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(itemIdx = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(itemIdx: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(data = "https://developer.android.com/images/brand/Android_Robot.png"),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = "Item #$itemIdx", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Preview
+@Composable
 fun TopAppBarBtn() {
     TopAppBar(
         title = {
             Text(text = "Page title", maxLines = 2)
         },
-        navigationIcon = {  }
+        navigationIcon = { }
     )
 }
 
